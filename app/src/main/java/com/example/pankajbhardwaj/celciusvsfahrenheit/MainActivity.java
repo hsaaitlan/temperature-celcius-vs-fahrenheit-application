7618b0c567f15c5c9a532b754a195c6522e2720b
hsaaitlan <pankajbhardwaj1798@gmail.com>
2019-06-06 21:20:14 +0530

    Third commit

--- app/src/main/java/com/example/pankajbhardwaj/celciusvsfahrenheit/MainActivity.java
+++ app/src/main/java/com/example/pankajbhardwaj/celciusvsfahrenheit/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,11 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = (Button) findViewById(R.id.btn);
+        Button btn = (Button) findViewById(R.id.ftoc);
+        Button btnn = (Button) findViewById(R.id.ctof);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i("myApp","Inside on click function");
+                int ans = 0;
+                EditText etxt = (EditText) findViewById(R.id.editText);
+                int f = Integer.parseInt(etxt.getText().toString());
+                double c = (f - 32) * 5 / 9.0;
+                TextView txt = (TextView) findViewById(R.id.rlt);
+                txt.setText(String.format("%.3f",c));
+            }
+        });
+        btnn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("myApp","Inside on click function");
+                int ans = 0;
+                EditText etxt = (EditText) findViewById(R.id.editText);
+                int c = Integer.parseInt(etxt.getText().toString());
+                double f = 1.8 * c + 32;
+                TextView txt = (TextView) findViewById(R.id.rlt);
+                txt.setText(String.format("%.3f",f));
             }
         });
     }
